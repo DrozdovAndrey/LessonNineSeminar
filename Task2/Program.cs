cf6aabe09498aa66988dc360658ecaa2946b0fd0
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-21 22:31:16 +0300

    task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,20 @@
-﻿
+﻿// Задача 2. Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты
+// Например, изначально массив выглядел вот так:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// Новый массив будет выглядеть вот так:
+
+// 1 4 7 2
+
+// 5 81 2 9
+
+// 8 4 2 4
+
 
 
 
@@ -14,9 +30,11 @@ namespace Seminar9
             FillArray(array);
             Console.WriteLine($"Начальный массив:  ");
             PrintArray(array);
+            Console.WriteLine();
+            Console.WriteLine($"{GetMinIndex(array)[0]} {GetMinIndex(array)[1]}");
+
             Console.WriteLine("Перевернутый массив: ");
-            ReplacLine(array);
-            PrintArray(array);
+            PrintArray(DeleteColStr(array, GetMinIndex(array)));
         }
         static void FillArray(int[,] array)
         {
@@ -28,20 +46,39 @@ namespace Seminar9
                 }
             }
         }
-        static void ReplacLine(int[,] array)
+        public static int[] GetMinIndex(int[,] array)
         {
-            int lineEnd = array.GetLength(0) - 1;
-            for (int i = 0; i < 1; i++)
+            int[] minIndex = new int[2];
+            int minI = 0;
+            int minJ = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    int number = array[i, j];
-                    array[i, j] = array[lineEnd, j];
-                    array[lineEnd, j] = number;
+                    if(array[i,j] < array[minI,minJ]) 
+                    {
+                        minI = i;
+                        minJ = j;
+                        minIndex[0] = i;
+                        minIndex[1] = j;
+                    }
                 }
-
             }
-
+            return minIndex;
+        }
+        static int[,] DeleteColStr(int[,] array, int[] minIndex)
+        {
+            int[,] number = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+            for (int i = 0, m = 0; i < array.GetLength(0) && m < number.GetLength(0); i++, m++)
+            {
+                if(i == minIndex[0] ) i++;
+                for (int j = 0, n = 0; j < array.GetLength(1) && n < number.GetLength(1); j++, n++)
+                {
+                    if(j == minIndex[1]) j++;
+                    number[m,n] = array[i,j];
+                }
+            }
+            return number;
         }
         static void PrintArray(int[,] array)
         {
