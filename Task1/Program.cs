804f47424111cb8365fbfc8cb9ae1d5d1b586f36
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-21 20:07:14 +0300

    create task1

---
+++ Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+
+namespace Seminar9
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            int n = new Random().Next(3, 7);
+            int m = new Random().Next(3, 7);
+            int[,] array = new int[m, n];
+            FillArray(array);
+            Console.WriteLine($"Начальный массив:  ");
+            PrintArray(array);
+            Console.WriteLine("Перевернутый массив: ");
+            ReplacLine(array);
+            PrintArray(array);
+        }
+        static void FillArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(0, 10);
+                }
+            }
+        }
+        static void ReplacLine(int[,] array)
+        {
+            int lineEnd = array.GetLength(0) - 1;
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    int number = array[i, j];
+                    array[i, j] = array[lineEnd, j];
+                    array[lineEnd, j] = number;
+                }
+
+            }
+
+        }
+        static void PrintArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
